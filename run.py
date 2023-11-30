c4c5618b9dfcdfd5f6c6c9a05e56be76910ef447
Imahn <imahn.shekhzadeh@hesge.ch>
2023-11-30 11:15:55 +0100

    remove not needed 'error_file_name' variable

--- run.py
+++ run.py
@@ -33,13 +33,9 @@ for run_id in range(num_runs):
     submit_file_name = f"run_dgp_{date_time[-8:]}_{run_id}.sh"
 
     # Define output and error file names
-    output_file_name = os.path.join(
+    log_file = os.path.join(
         saving_path__slurm,
         f"run_%j_{date_time[-8:]}_{run_id}.log",
-    )
-    error_file_name = os.path.join(
-        saving_path__slurm,
-        f"run_%j_{date_time[-8:]}_{run_id}.err",
     )
 
     with open(
@@ -55,8 +51,8 @@ for run_id in range(num_runs):
             "benchmarking\n"
         )
         file.write(f"#SBATCH --job-name=dgp_{run_id}\n")
-        file.write(f"#SBATCH --output={output_file_name}\n")
-        file.write(f"#SBATCH --error={output_file_name}\n")
+        file.write(f"#SBATCH --output={log_file}\n")
+        file.write(f"#SBATCH --error={log_file}\n")
         # file.write(f'#SBATCH --mail-type=END\n')
         # file.write(f'#SBATCH --mail-user=imahn.shekhzadeh@unige.ch\n')
         file.write(f"#SBATCH --mem-per-cpu=8G\n")  # minimum memory per CPU
