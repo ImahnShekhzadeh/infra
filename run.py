e5ef49d92ff9c0a33b6f7b1652a866f5c122c751
Imahn <imahn.shekhzadeh@hesge.ch>
2024-01-23 12:43:39 +0100

    refactor `run.py`

--- run.py
+++ run.py
@@ -45,59 +45,60 @@ def write_skeleton(
         f.write(f"#SBATCH --wait-all-nodes=1\n\n")
 
 
-# Parameter
-num_runs = 200
+if __name__ == "__main__":
+    # Parameter
+    num_runs = 200
 
-# Hyperparameter
-num_samples = 100  # int(1e3)
+    # Hyperparameter
+    num_samples = 100  # int(1e3)
 
-# Retrieve current time and day
-date_time = dt.now().strftime("%dp%mp%y_%Hp%Mp%S")
-day = date_time[:8]
+    # Retrieve current time and day
+    date_time = dt.now().strftime("%dp%mp%y_%Hp%Mp%S")
+    day = date_time[:8]
 
-# Define saving path for slurm files
-saving_path__slurm = os.getcwd().split("/")
-saving_path__slurm.pop(-1)
-saving_path__slurm = "/".join(saving_path__slurm)
-saving_path__slurm = os.path.join(saving_path__slurm, "slurm", "dgp", day)
+    # Define saving path for slurm files
+    saving_path__slurm = os.getcwd().split("/")
+    saving_path__slurm.pop(-1)
+    saving_path__slurm = "/".join(saving_path__slurm)
+    saving_path__slurm = os.path.join(saving_path__slurm, "slurm", "dgp", day)
 
-# Define saving path for submit files
-saving_path__submit = os.path.join(os.getcwd(), day)
+    # Define saving path for submit files
+    saving_path__submit = os.path.join(os.getcwd(), day)
 
-# Create folder for slurm and files
-os.makedirs(saving_path__slurm, exist_ok=True)
-os.makedirs(saving_path__submit, exist_ok=True)
+    # Create folder for slurm and files
+    os.makedirs(saving_path__slurm, exist_ok=True)
+    os.makedirs(saving_path__submit, exist_ok=True)
 
-# Write `num_runs` submit files
-for run_id in range(num_runs):
-    # Define submit file and log file
-    submit_file = os.path.join(
-        saving_path__submit, f"run_dgp_{date_time[-8:]}_{run_id}.sh"
-    )
-    log_file = os.path.join(
-        saving_path__slurm,
-        f"run_%j_{date_time[-8:]}_{run_id}.log",
-    )
-
-    # Write skeleton for submit file
-    write_skeleton(
-        submit_file=submit_file,
-        job_name="dgp",
-        log_file=log_file,
-        partition="shared-cpu",
-        python_dir="/home/users/s/shekhza2/ant-migrate/git_repo/benchmarking",
-        run_id=run_id,
-    )
-
-    # append rest
-    with open(submit_file, "a") as f:
-        f.write(
-            "module purge && module load GCC/11.3.0 OpenMPI/4.1.4 geopsy/3.4." "2\n"
+    # Write `num_runs` submit files
+    for run_id in range(num_runs):
+        # Define submit file and log file
+        submit_file = os.path.join(
+            saving_path__submit, f"run_dgp_{date_time[-8:]}_{run_id}.sh"
         )
-        f.write("source activate ant-migrate-dev\n")
-        f.write(
-            f"python -m scripts_python.fwd_model.generate_data -i {run_id} -s "
-            f"-n {num_samples} -u\n"
+        log_file = os.path.join(
+            saving_path__slurm,
+            f"run_%j_{date_time[-8:]}_{run_id}.log",
         )
 
-    subprocess.Popen(["sbatch", submit_file])
+        # Write skeleton for submit file
+        write_skeleton(
+            submit_file=submit_file,
+            job_name="dgp",
+            log_file=log_file,
+            partition="shared-cpu",
+            python_dir="/home/users/s/shekhza2/ant-migrate/git_repo/benchmarking",
+            run_id=run_id,
+        )
+
+        # append rest
+        with open(submit_file, "a") as f:
+            f.write(
+                "module purge && module load GCC/11.3.0 OpenMPI/4.1.4 geopsy/3.4." "2\n"
+            )
+            f.write("source activate ant-migrate-dev\n")
+            f.write(
+                f"python -m scripts_python.fwd_model.generate_data -i {run_id} -s "
+                f"-n {num_samples} -u\n"
+            )
+
+        subprocess.Popen(["sbatch", submit_file])
